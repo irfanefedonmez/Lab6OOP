7eb8c7723a6c16c6d90392db3eb50f7f707cd195
https://github.com/irfanefedonmez/Oop_exam_summer_50.git <g00472272@atu.ie>
2026-03-16 12:12:51 +0000

    improve controller

--- src/main/java/atu/ie/week8_reservations/controller/ReservationController.java
+++ src/main/java/atu/ie/week8_reservations/controller/ReservationController.java
@@ -1,4 +1,21 @@
 package atu.ie.week8_reservations.controller;
 
+
+import atu.ie.week8_reservations.model.Reservation;
+import atu.ie.week8_reservations.service.ReservationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rezervations")
 public class ReservationController {
+    private ReservationService reservationService;
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {}
 }
