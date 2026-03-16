74f16d636f253223571446cceaa53cd7bea48b1e
https://github.com/irfanefedonmez/Oop_exam_summer_50.git <g00472272@atu.ie>
2026-03-16 11:26:11 +0000

    Initial commit

---
+++ src/main/java/atu/ie/week8_reservations/Lab6OopApplication.java
@@ -0,0 +1,13 @@
+package atu.ie.week8_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6OopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab6OopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/week8_reservations/Lab6OopApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.week8_reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6OopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
