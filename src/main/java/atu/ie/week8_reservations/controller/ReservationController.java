f9012a67e98976000bffb75ee55f1c43fdf1838c
irfanefedonmez <g00472272@atu.ie>
2026-03-18 22:39:55 +0000

    controller done

--- src/main/java/atu/ie/week8_reservations/controller/ReservationController.java
+++ src/main/java/atu/ie/week8_reservations/controller/ReservationController.java
@@ -1,21 +1,37 @@
 package atu.ie.week8_reservations.controller;
 
-
 import atu.ie.week8_reservations.model.Reservation;
 import atu.ie.week8_reservations.service.ReservationService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/rezervations")
+@RequestMapping("/reservations")
 public class ReservationController {
-    private ReservationService reservationService;
+
+    private final ReservationService reservationService;
+
     public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 
     @PostMapping
-    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {}
+    public ResponseEntity<Reservation> addReservation(@Valid @RequestBody Reservation reservation) {
+        Reservation savedReservation = reservationService.addReservation(reservation);
+        return new ResponseEntity<>(savedReservation, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getAllReservations() {
+        return new ResponseEntity<>(reservationService.getAllReservations(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> getReservationById(@PathVariable Long id) {
+        return new ResponseEntity<>(reservationService.getReservationById(id), HttpStatus.OK);
+    }
 }
